3cd9cef204b93c099b36235f7872791c67128f20
jhon-crypt <johnoladele690@gmail.com>
2023-09-07 18:01:19 +0100

    Made final updates

---
+++ src/components/hero/headerHero.jsx
@@ -0,0 +1,25 @@
+export default function HeaderHero() {
+
+    return (
+
+        <>
+
+            <section>
+                <div className="bg-blueGray-100 rounded-b-9xl">
+                    <div className="container px-4 mx-auto">
+                        <div className="relative pt-24 mb-16 xl:mb-0 z-20">
+                            <span className="block mb-9 font-medium tracking-widest uppercase text-center text-sm text-gray-300">Better communication and work</span>
+                            <h1 className="max-w-lg mx-auto font-heading font-medium text-center text-5xl md:text-7xl leading-tight">Kreate Your Vision Online</h1>
+                        </div>
+                    </div>
+                    <div className="relative xl:-mt-32 max-w-max">
+                        <img src="https://shuffle.dev/uinel-assets/images/heroes/widget2.png" alt=""/>
+                    </div>
+                </div>
+            </section>
+
+        </>
+
+    )
+
+}
---
+++ src/components/section/callToAction.jsx
@@ -0,0 +1,21 @@
+export default function CallToAction() {
+
+    return (
+
+        <>
+
+            <section className="relative py-24 2xl:py-44 font-heading font-medium text-white bg-indigo-700 overflow-hidden rounded-3xl">
+                <div className="relative container px-4 mx-auto z-10 text-center">
+                    <span className="inline-block mb-10 py-3 px-7 font-medium text-lg leading-5 border border-white rounded-3xl">Join us today</span>
+                    <h2 className="mb-14 xl:mb-16 font-heading text-5xl md:text-10xl xl:text-11xl leading-tight">Join over 1 mln designers</h2>
+                    <p className="mb-14 xl:mb-16 font-sans font-normal text-base leading-6">A short message that will bring your customers into the world.</p>
+                    <a className="text-black inline-block py-5 px-10 text-xl leading-6 font-medium tracking-tighter font-heading bg-white hover:bg-gray-50 text-body focus:ring-2 focus:ring-white focus:ring-opacity-50 rounded-xl" href="#">Join now</a>
+                </div>
+                <img className="absolute top-0 left-1/2 transform -translate-x-1/2 md:px-6 h-full" src="https://shuffle.dev/uinel-assets/elements/call-to-action/elipse-bg-blue.svg" alt=""/>
+            </section>
+
+        </>
+
+    )
+
+}
